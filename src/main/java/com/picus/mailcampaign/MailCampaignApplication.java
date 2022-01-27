63764cdd1e1b77fc43d6818499075bd3d1da7ad6
yavuz.kaya <kyavuz96@gmail.com>
2022-01-28 00:20:50 +0300

    initial commit

---
+++ src/main/java/com/picus/mailcampaign/MailCampaignApplication.java
@@ -0,0 +1,13 @@
+package com.picus.mailcampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCampaignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailCampaignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picus/mailcampaign/MailCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picus.mailcampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCampaignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
